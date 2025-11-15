1b0ec3f75c98051252f0b7ba3bc3849c243af233
Nursultan <zhonurs@gmail.com>
2025-11-15 16:35:44 +0500

    Add frame

--- gstreame.py
+++ gstreame.py
@@ -1,7 +1,8 @@
 import cv2
 
-rtsp_url = "rtsp://admin:nurs1905@192.168.1.64:554/Streaming/Channels/101"
+rtsp_url = "rtsp://admin:password@192.168.1.64:554/Streaming/Channels/101"
 
+# Отключаем буфер (важно!)
 gst = (
     f"rtspsrc location={rtsp_url} latency=0 ! "
     "rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink"
@@ -9,13 +10,21 @@ gst = (
 
 cap = cv2.VideoCapture(gst, cv2.CAP_GSTREAMER)
 
+if not cap.isOpened():
+    print("❌ Не могу открыть RTSP поток")
+    exit()
+
+print("✅ RTSP открыт! Начинаю чтение кадров...")
+
 while True:
     ret, frame = cap.read()
-    if not ret:
+
+    if not ret or frame is None:
         print("no frame")
         continue
 
-    cv2.imshow("Camera", frame)
+    cv2.imshow("IP Camera", frame)
+
     if cv2.waitKey(1) == 27:
         break
 
